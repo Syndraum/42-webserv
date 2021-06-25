2f41f123a1e3dc0109235d8e6fddae5e14d3618c
mchardin <mchardin@student.42.fr>
2021-06-25 18:05:21 +0200

    feat: redesigned configuration parsing in a builder, cleared code, added fonctions, solved errors, not tested yet

---
+++ CoreBuilder.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   CoreBuilder.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchardin <mchardin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/23 17:04:45 by mchardin          #+#    #+#             */
+/*   Updated: 2021/06/25 17:55:16 by mchardin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Core.hpp"
+#include <cctype>
+
+class CoreBuilder
+{
+	private:
+
+		size_t		_idx;
+		std::string	_line;
+		Core		*_core;
+
+		void	skip_whitespaces();
+		void	skip_comments();
+		void	parse_server_port(Server *server);
+		void	parse_server_string(Server *server, int directive_len, void (*setter)(std::string));
+		void	parse_server();
+		void	parse_worker();
+
+	public:
+		CoreBuilder(std::istream fd, Core *core);
+		~CoreBuilder();
+		Core	*get_builded_core() const;
+};
