9dea2280ba7309f775fea7627221f3c12bac63d4
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2021-07-03 13:16:27 +0000

    clean <main>: delete comment

--- main.cpp
+++ main.cpp
@@ -45,48 +45,4 @@ int		main(int argc, char *argv[])
 	}
 	
 	core.start();
-
-//maia's part
-// #include "includes.hpp"
-// #include "BuilderRequest.hpp"
-
-// std::string	error_msg(int code) // ADD TO RESPONSE
-// {
-// 	if (code == BAD_REQUEST)
-// 		return ("Bad Request");
-// 	else if(code == BAD_VERSION)
-// 		return ("HTTP Version Not Supported");
-// 	else if(code == NOT_IMPLEMENTED)
-// 		return("Not Implemented");
-// 	return (0);
-// }
-
-// int		print_error(int code) // ADD TO RESPONSE
-// {
-// 	std::cout << code << " " << error_msg(code) << std::endl;
-// 	return(code);
-// }
-
-// int		main() // run with "cat test_request | ./webserv"
-// {
-// 	BuilderRequest bd;
-// 	Request	*	request = 0;
-// 	// int			code;
-
-// 	try
-// 	{
-// 		bd.parse_request(std::cin);
-// 		request = bd.get_request();
-// 		bd.reset();
-// 	}
-// 	catch(const std::exception& e)
-// 	{
-// 		std::cerr << e.what() << '\n';
-// 	}
-// 	// code = parse_request(std::cin, &request);
-// 	// if (code != OK)
-// 	// 	return(print_error(code));
-// 	if (request)
-// 		request->print_debug();
-// 	delete request;
 }
