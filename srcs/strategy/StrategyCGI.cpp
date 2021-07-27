77b8241ef29d48fc0dd4235a0996a6a2f1eed4aa
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2021-07-27 11:08:35 +0000

    fix: add header Request CGI

--- srcs/strategy/StrategyCGI.cpp
+++ srcs/strategy/StrategyCGI.cpp
@@ -56,30 +56,29 @@ StrategyCGI::create(Client & client)
 void
 StrategyCGI::_prepare(Client & client)
 {
-	// Request &	request_http	= client.get_request();
-	// Server &	server			= client.get_server();
+	Request &	request_http	= client.get_request();
+	Server &	server			= client.get_server();
 
-	(void)client;
-	// if (request_http.get_body().length() != 0){
-	// 	_request.add_header("CONTENT_LENGTH", 0);
-	// 	if (request_http.has_header("Content-Type"))
-	// 		_request.add_header("CONTENT_TYPE", request_http.get_header("Content-Type"));
-	// }
-	// if (_request.get_header("PATH_INFO") != "")
-	// 	_request.add_header("PATH_TRANSLATED", server.get_full_path(request_http.get_uri().get_extra_path()));
-	// _request
-	// 	.add_header("AUTH_TYPE", "")
-	// 	.add_header("GATEWAY_INTERFACE", Info::cgi_revision)
-	// 	.add_header("PATH_INFO", request_http.get_uri().get_extra_path())
-	// 	.add_header("QUERY_STRING", request_http.get_uri().get_query_string())
-	// 	.add_header("REMOTE_ADDR", "0.0.0.0") // TMP
-	// 	.add_header("REMOTE_HOST", "")
-	// 	.add_header("REQUEST_METHOD", request_http.get_method()->get_name())
-	// 	.add_header("SCRIPT_NAME", request_http.get_uri().get_path())
-	// 	.add_header("SERVER_NAME", "0.0.0.0") // TMP
-	// 	.add_header("SERVER_PORT", client.get_server_socket().get_port())
-	// 	.add_header("SERVER_PROTOCOL", Info::http_revision)
-	// 	.add_header("SERVER_SOFTWARE", Info::server_name + "/" + Info::version)
-	// ;
+	if (request_http.get_body().length() != 0){
+		_request.add_header("CONTENT_LENGTH", 0);
+		if (request_http.has_header("Content-Type"))
+			_request.add_header("CONTENT_TYPE", request_http.get_header("Content-Type"));
+	}
+	_request
+		.add_header("AUTH_TYPE", "")
+		.add_header("GATEWAY_INTERFACE", Info::cgi_revision)
+		.add_header("PATH_INFO", request_http.get_uri().get_extra_path())
+		.add_header("QUERY_STRING", request_http.get_uri().get_query_string())
+		.add_header("REMOTE_ADDR", "0.0.0.0") // TMP
+		.add_header("REMOTE_HOST", "")
+		.add_header("REQUEST_METHOD", request_http.get_method()->get_name())
+		.add_header("SCRIPT_NAME", request_http.get_uri().get_path())
+		.add_header("SERVER_NAME", "0.0.0.0") // TMP
+		.add_header("SERVER_PORT", client.get_server_socket().get_port())
+		.add_header("SERVER_PROTOCOL", Info::http_revision)
+		.add_header("SERVER_SOFTWARE", Info::server_name + "/" + Info::version)
+	;
+	if (_request.get_header("PATH_INFO") != "")
+		_request.add_header("PATH_TRANSLATED", server.get_full_path(request_http.get_uri().get_extra_path()));
 	// _request.add_header("SCRIPT_FILENAME", server.get_full_path(request_http.get_uri().get_path())); //PHP
 }
