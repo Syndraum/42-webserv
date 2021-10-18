53e985d116c2af68a19cd10625c9148f61ee51d4
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2021-10-18 14:01:25 +0000

    style: add information on form files

--- html/form_get.php
+++ html/form_get.php
@@ -1,37 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-
-    <!-- Declared Vars To Go Here -->
-
+<head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Metadata -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Page Name and Site Name -->
     <title>Form</title>
 
     <!-- CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <link href="mysource_files/style.css" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="assets/style.css" rel="stylesheet">
   </head>
 
   <body>
-
-    <div class="container">
-
-		<h3>GET TEST</h3>
-		<div>
-			Email : <?php echo $_GET['email'] ?>
-			<br>
-			Login : <?php echo $_GET['login'] ?>
+	<div class="container-fluid">
+		<div class="row justify-content-center">
+			<div class="col-11 col-lg-9">
+				<div class="row mt-5">
+					<div class="col-12">
+						<a href="/"><h1 class="fs-1 fw-bold">Webserviette</h1></a>
+					</div>
+				</div>
+				<div class="row mt-3">
+					<div class="col-9 mb-5">
+						<h3>GET Form : result</h3>
+						<p>
+							Les données du formulaire s'affiche si dessous. Observez bien l'url. Il se compose de la ressource, d'un <span class="code-text">?</span>, et d'argument au format <span class="code-text">nom=valeur</span><br>
+							Essayez de changer ses valeurs à la main et relancez la requête.
+						</p>
+						<div class="code">
+							Email : <span class="fw-light"><?php echo $_GET['email'] ?></span>
+							<br>
+							Login : <span class="fw-light"><?php echo $_GET['login'] ?></span>
+						</div>
+						<a class="link d-inline-flex flex-row align-items-center mt-5" href="/form.html">
+							<span class="link-text me-5 fs-5">Retour au fromulaire</span>
+							<svg class="link-arrow" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#0437ce" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+								<line x1="4" y1="12" x2="20" y2="12"/>
+								<polyline points="14 6 20 12 14 18"/>
+							</svg>
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
-    </div> <!-- /container -->
-
-  </body>
+	</div>
+</body>
 </html>
