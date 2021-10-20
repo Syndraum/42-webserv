2c3f2dfe8fff071a25ce5e975a9df6c333fb412a
Romain ALVAREZ <roalvare@e2r11p3.42.fr>
2021-10-20 08:38:21 +0000

    feat: add page subdir and php info

--- html/form_get.php
+++ html/form_get.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Form GET</title>
 
     <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="assets/style.css" rel="stylesheet">
+    <link href="/assets/style.css" rel="stylesheet">
   </head>
 
   <body>
--- html/index.php
+++ html/index.php
@@ -1,6 +1,2 @@
 <?php
-$test = 0;
-
-echo "test\n";
-// var_dump($_SERVER);
 phpinfo();
